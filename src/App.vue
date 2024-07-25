e555a3aeaeab20a50ceafbe1257f24348e355d73
kirillmihalych <tiltlessman@gmail.com>
2024-07-25 11:56:50 +0300

    [1] css reset

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="base">hello there</header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+@import './assets/style.scss';
+</style>
